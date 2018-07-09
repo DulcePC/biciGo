75130103814408848770be28f8ba173da9a819d3
Dulce <dulcefrancelispc@gmail.com>
2018-07-09 13:19:24 -0400

    Slide
    agregamos el primer section view de nuestro index

---
+++ view/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>biciGo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/materialize.css" />
+    <link rel="stylesheet" href="css/index.css" />
+
+</head>
+<body>
+    <nav>
+        <div class="nav-wrapper">
+        <a href="#" class="brand-logo"><img src="img/logo.jpg" class="logo"></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Inicio</a></li>
+            <li><a href="badges.html">Categorias</a></li>
+            <li><a href="collapsible.html">Nosotros</a></li>
+            <li><a href="collapsible.html">Noticias</a></li>
+            <li><a href="collapsible.html">Eventos</a></li>
+        </ul>
+        </div>
+    </nav>
+    <section id="wrapper" class="skewed">
+        <div class="layer bottom">
+            <div class="content-wrap">
+                <div class="content-body">
+                    <h1>Piezas</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <a class="btn orange">Categorias</a>
+                </div>
+                <img src="img/bici2.jpg">
+            </div>
+        </div>
+        <div class="layer top">
+            <div class="content-wrap">
+                <div class="content-body ">
+                    <h1>BiciGo</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                    <a class="btn green">Subscribete</a>
+                </div>
+                <img src="img/bici1.jpeg">
+            </div>
+        </div>
+        <div class="handle"></div>
+    </section>
+    <div class="red">
+        <br>
+        <br>
+        <br>
+        <br>
+    </div>
+
+
+
+
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
+    <script src="js/index.js"></script>    
+</body>
+</html>
