2c4085dd7577580f479590b77520cd4254272440
Dulce <dulcefrancelispc@gmail.com>
2018-07-11 09:05:32 -0400

    Contacto fast
    Comentario en el codigo de lo que haremos con el menu de arriba

--- view/index.php
+++ view/index.php
@@ -5,30 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>biciGo</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="css/materialize.css" />
     <link rel="stylesheet" href="css/index.css" />
 
 </head>
 <body>
-    <nav>
+    <!-- creo que eliminaremos esto porque no se vuelve responsive, o lo escondemos para para movil o lo hacemos con flexbox ( a trabajar)-->
+    <div class="submenu">
+       <div class="container">
+           <ul class="col l12">
+               <li class="col s3"><a><i class="material-icons tiny">lock_open</i>Entra</a></li>
+               <li class="col s3"><a><i class="material-icons tiny">lock_open</i>Registrate<a></li>
+               <li class="col s3"><a><i class="material-icons tiny">phone</i>Llamanos: 123456789</a></li>
+               <li class="col s3"><a><i class="material-icons tiny">mail</i>bicigo@gmail.com</a></li>
+           </ul>
+       </div>
+    </div>
+    <nav class="navprincipal"> 
         <div class="nav-wrapper">
-        <a href="#" class="brand-logo"><img src="img/logo.jpg" class="logo"></a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li><a href="sass.html">Inicio</a></li>
-            <li><a href="badges.html">Categorias</a></li>
-            <li><a href="collapsible.html">Nosotros</a></li>
-            <li><a href="collapsible.html">Noticias</a></li>
-            <li><a href="collapsible.html">Eventos</a></li>
-        </ul>
+            <a href="#" class="brand-logo">BiciGo</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Inicio</a></li>
+                <li><a href="badges.html">Categorias</a></li>
+                <li><a href="collapsible.html">Nosotros</a></li>
+                <li><a href="collapsible.html">Noticias</a></li>
+                <li><a href="collapsible.html">Eventos</a></li>
+            </ul>
         </div>
     </nav>
     <section id="wrapper" class="skewed">
         <div class="layer bottom">
             <div class="content-wrap">
-                <div class="content-body">
+                <div class="content-body col l12">
                     <h1>Piezas</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                    <a class="btn orange">Categorias</a>
+                    <a class="btn orange col s3">Categorias</a>
                 </div>
                 <img src="img/bici2.jpg">
             </div>
