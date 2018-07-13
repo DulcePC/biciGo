ecd58c5e4cb8fc7c534fa4426d9a38e9d17efa46
Dulce <dulcefrancelispc@gmail.com>
2018-07-13 01:35:32 -0400

    Secciones
    Secciones terminadas

--- view/index.php
+++ view/index.php
@@ -39,7 +39,7 @@
                 <div class="content-body col l12">
                     <h1>Piezas</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                    <a class="btn orange col s3">Categorias</a>
+                    <a class="btn btn-small orange col s3">Categorias</a>
                 </div>
                 <img src="img/bici2.jpg">
             </div>
@@ -49,7 +49,7 @@
                 <div class="content-body ">
                     <h1>BiciGo</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                    <a class="btn green">Subscribete</a>
+                    <a class="btn btn-small green">Subscribete</a>
                 </div>
                 <img src="img/bici1.jpeg">
             </div>
@@ -57,7 +57,7 @@
         <div class="handle"></div>
     </section>
     <div class="ventas">
-        <div class="container">
+        <div class="container titulo">
             <h3>Ventas</h3>
             <h6>Encuentra tu bicicleta ahora</h6>
         </div>
@@ -114,10 +114,10 @@
                         </div>
                 </div>
                 <div class="col l4  m4 s12">
-                        <div class="card">
+                        <div class="card ">
                             <div class="card-image">
                             <img src="img/bici3.jpg">
-                            <span class="card-title">Card Title</span>
+                            <span class="card-title">what</span>
                             <a class="btn-floating halfway-fab waves-effect waves-light card-butttoms"><i class="material-icons">add</i></a>
                             </div>
                             <div class="card-content">
@@ -126,7 +126,7 @@
                         </div>
                 </div>
                 <div class="col l4 m4 s12">
-                        <div class="card">
+                    <div class="card">
                             <div class="card-image">
                             <img src="img/logo.jpg">
                             <span class="card-title">Card Title</span>
@@ -135,24 +135,24 @@
                             <div class="card-content">
                             <p>I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.</p>
                             </div>
-                        </div>
+                    </div>
                 </div>            
             </div>
-        </div>
-        <div class="paginacion">
-            <ul class="pagination container">
-                <li class="disabled izquierda"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
-                <li class="active"><a href="#!">1</a></li>
-                <li class="waves-effect"><a href="#!">2</a></li>
-                <li class="waves-effect"><a href="#!">3</a></li>
-                <li class="waves-effect"><a href="#!">4</a></li>
-                <li class="waves-effect"><a href="#!">5</a></li>
-                <li class="waves-effect derecha"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
-            </ul>
+            <div class="row">
+                    <ul class="pagination right">
+                        <li class="disabled izquierda"><a href="#!"><i class="material-icons">chevron_left</i></a></li>
+                        <li class="active"><a href="#!">1</a></li>
+                        <li class="waves-effect"><a href="#!">2</a></li>
+                        <li class="waves-effect"><a href="#!">3</a></li>
+                        <li class="waves-effect"><a href="#!">4</a></li>
+                        <li class="waves-effect"><a href="#!">5</a></li>
+                        <li class="waves-effect derecha"><a href="#!"><i class="material-icons">chevron_right</i></a></li>
+                    </ul>
+            </div>
         </div>
     </div>
     <div class="parallax-container">
-      <div class="parallax"><img src="img/bici2.jpg"></div>
+      <div class="parallax"><img src="img/bici8.jpg"></div>
     </div>
     <section class="nosotros">
         <div class="container">
@@ -162,46 +162,139 @@
             </div>
         </div>
        <div class="row">
-           <div class="col l6 m6 s12 pink">
-               <div class="contenedor">
-                   <div class="texto">
-                       <h3>Nosotros</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, beatae at quibusdam nihil ad libero nemo dolore dolorum mollitia, quidem, temporibus eligendi et corporis magni voluptatem! Amet ea voluptatibus suscipit aperiam quod nihil harum repellendus? Quasi earum labore adipisci nobis voluptatem provident dignissimos. Nesciunt ad, aperiam temporibus suscipit facilis maiores.</p>
-                   </div>
-               </div>
-           </div>
-           <div class="col l6 m6 s12 blue">
-           <div class="contenedor">
-                   <div class="texto">
-                       <h3>IMGAGEN</h3>
-                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, beatae at quibusdam nihil ad libero nemo dolore dolorum mollitia, quidem, temporibus eligendi et corporis magni voluptatem! Amet ea voluptatibus suscipit aperiam quod nihil harum repellendus? Quasi earum labore adipisci nobis voluptatem provident dignissimos. Nesciunt ad, aperiam temporibus suscipit facilis maiores.</p>
-                   </div>
-               </div>
-           </div>
+           <div class="container">
+                <div class="col l12 m12 s12">
+                    <div class="contenedor">
+                        <div class="texto">
+                            <h3>BiciGo</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, beatae at quibusdam nihil ad libero nemo dolore dolorum mollitia, quidem, temporibus eligendi et corporis magni voluptatem! Amet ea voluptatibus suscipit aperiam quod nihil harum repellendus? Quasi earum labore adipisci nobis voluptatem provident dignissimos. Nesciunt ad, aperiam temporibus suscipit facilis maiores.</p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, beatae at quibusdam nihil ad libero nemo dolore dolorum mollitia, quidem, temporibus eligendi et corporis magni voluptatem! Amet ea voluptatibus suscipit aperiam quod nihil harum repellendus? Quasi earum labore adipisci nobis voluptatem provident dignissimos. Nesciunt ad, aperiam temporibus suscipit facilis maiores.</p>
+                        </div>
+                        <a class="green btn btn-small right" id="suscribete">Suscribete</a>
+                    </div>
+                </div>
+            </div>
        </div>
     </section>
     <div class="parallax-container">
-      <div class="parallax"><img src="img/bici2.jpg"></div>
+      <div class="parallax"><img src="img/bici10.jpg"></div>
     </div>
-    <section class="actividades">
+    <section class="actividades ">
         <div class="noticias">
-            <div class="actividadesTitulo">
+            <div class="titulo">
                 <h4>Noticias</h4>
+            </div>
+            <div class="container contenedorEventos">
+                <div class="row">
+                    <div class="col l6 m6 s6">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="img/bici11.jpg">
+                            </div>
+                            <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Compentencia de EEUU ride<i class="material-icons right">more_vert</i></span>
+                            <p><a href="#">Ver mas</a></p>
+                            </div>
+                            <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col l6 m6 s6">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="img/bici11.jpg">
+                            </div>
+                            <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Resultados de biciclismo mundial<i class="material-icons right">more_vert</i></span>
+                            <p><a href="#">Ver mas</a></p>
+                            </div>
+                            <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="eventos">
-            <div class="actividadesTitulo">
+            <div class="titulo">
                 <h4>Eventos</h4>
+            </div>
+            <div class="container contenedorEventos">
+                <div class="row">
+                    <div class="col l6 m6 s6">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="img/bici11.jpg">
+                            </div>
+                            <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Corre por la vida<i class="material-icons right">more_vert</i></span>
+                            <p><a href="#">Ver mas</a></p>
+                            </div>
+                            <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col l6 m6 s6">
+                        <div class="card">
+                            <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="img/bici11.jpg">
+                            </div>
+                            <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4">Corre por dinero<i class="material-icons right">more_vert</i></span>
+                            <p><a href="#">Ver mas</a></p>
+                            </div>
+                            <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
+                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
-    <div class="parallax-container">
-      <div class="parallax"><img src="img/bici3.jpg"></div>
-    </div>
-    <footer>
-
-
-    </footer>
+    <footer class="page-footer col s12 l12 m12 black">
+        <div class="container">
+        <div class="row">
+            <div class="col l16 m6 12">
+                <div class="titulo">
+                    <h6>Grupo</h6>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam nesciunt autem excepturi vitae aliquid sit exercitationem velit enim animi labore.</p>
+                </div>
+            </div>
+            <div class="col l6 m6 s12">
+                <div class="titulo">
+                    <h6>Herramientas</h6>
+                    <ul class="center">
+                        <li><a class="white-text" href="#!">Materialize</a></li>
+                        <li><a class="white-text" href="#!">PHP</a></li>
+                        <li><a class="white-text" href="#!">HTML5</a></li>
+                        <li><a class="white-text" href="#!">CSS3</a></li>
+                        <li><a class="white-text" href="#!">Javascript</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col l12 m12 s12 center">
+                <a href="https://www.facebook.com/alicia.perez.79219"><img src="img/facebook.png" onmouseover="this.src='img/facebook2.png';" onmouseout="this.src='img/facebook.png';" /></a>
+                <a href="https://www.instagram.com/dulcep.c/?hl=en"><img src="img/instagram.png" onmouseover="this.src='img/instagram3.png';" onmouseout="this.src='img/instagram.png';" /></a>
+                <a href="https://www.linkedin.com/in/dulceperezsoft/"><img src="img/linkedin.png" onmouseover="this.src='img/linkedin2.png';" onmouseout="this.src='img/linkedin.png';" /></a>
+            </div>  
+        </div>
+        <div class="row nomargin">
+            <div class="col l12 m12 s12 center creditos">
+                <p>Powered by Dulce, with Materialize</p>
+            </div>
+        </div> 
+    </div>  
+</footer>
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
     <script src="js/index.js"></script>    
